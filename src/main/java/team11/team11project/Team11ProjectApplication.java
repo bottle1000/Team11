fd774c65e3d04aa16b1489860662e01349ef11c8
박병천 <genius178896@gmail.com>
2025-01-06 15:33:22 +0900

    초기 커밋

---
+++ src/main/java/team11/team11project/Team11ProjectApplication.java
@@ -0,0 +1,13 @@
+package team11.team11project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team11ProjectApplication.class, args);
+    }
+
+}
