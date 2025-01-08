0829aa059589a7fca2555242338e1bfaa217fd68
박병천 <genius178896@gmail.com>
2025-01-08 10:45:30 +0900

    fix : @EnableJpaAuditing 추가해줘서 Auditing 오류 해결

--- src/main/java/team11/team11project/Team11ProjectApplication.java
+++ src/main/java/team11/team11project/Team11ProjectApplication.java
@@ -2,8 +2,10 @@ package team11.team11project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Team11ProjectApplication {
 
     public static void main(String[] args) {
