4bbd232d1513988e453e715fbed4dfe1b18d1db3
박병천 <genius178896@gmail.com>
2025-01-10 17:52:38 +0900

    Feat : 리뷰 조회 기능 추가

--- src/main/java/team11/team11project/review/Service/ReviewServiceImpl.java
+++ src/main/java/team11/team11project/review/Service/ReviewServiceImpl.java
@@ -1,6 +1,8 @@
 package team11.team11project.review.Service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import team11.team11project.common.entity.Member;
@@ -8,16 +10,14 @@ import team11.team11project.common.entity.Orders;
 import team11.team11project.common.entity.Review;
 import team11.team11project.common.entity.Store;
 import team11.team11project.common.exception.NotFoundException;
+import team11.team11project.common.exception.UnauthorizedAccessException;
 import team11.team11project.order.repository.OrderRepository;
 import team11.team11project.review.dto.request.ReviewAddRequestDto;
 import team11.team11project.review.dto.response.ReviewAddResponseDto;
+import team11.team11project.review.dto.response.ReviewDto;
 import team11.team11project.review.repository.ReviewRepository;
 import team11.team11project.store.repository.StoreRepository;
 import team11.team11project.user.repository.MemberRepository;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,14 +28,21 @@ public class ReviewServiceImpl implements ReviewService {
     private final MemberRepository memberRepository;
     private final StoreRepository storeRepository;
 
-
     @Override
-    public ReviewAddResponseDto addReview(Long orderId, ReviewAddRequestDto dto) {
+    public ReviewAddResponseDto addReview(Long orderId, ReviewAddRequestDto dto, HttpServletRequest servletRequest) {
+
+        Long memberId = (Long) servletRequest.getAttribute("memberId");
+
         Orders order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new NotFoundException("찾는 주문이 없습니다."));
+
+        if (!order.getCustomer().getId().equals(memberId)) {
+            throw new UnauthorizedAccessException("이 주문에 대한 권한이 없습니다.");
+        }
+
         Store store = storeRepository.findById(orderId)
                 .orElseThrow(() -> new NotFoundException("찾는 가게가 없습니다."));
-        Member customer = memberRepository.findById(order.getCustomer().getId())
+        Member customer = memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundException("찾는 고객이 없습니다."));
 
         Review review = Review.createReview(dto, store ,order, customer);
@@ -44,17 +51,12 @@ public class ReviewServiceImpl implements ReviewService {
         return ReviewAddResponseDto.toDto(review);
     }
 
-    // FIX : GetReview를 없애고 getCreateAt으로 교체
+    // FIX : GetReviewTime를 없애고 getCreateAt으로 교체
     // TODO : 가게 정보를 다건으 조회는 하는데 리뷰를 별점 범위에 따라 조회. 따로 봐야하는건지 같이 봐야하는건지 잘 모르겠음.
     @Override
-    public List<ReviewAddResponseDto> findByReviewsById(Long storeId, int minRating, int maxRating, Pageable pageable) {
-        List<Review> reviews = reviewRepository.findAllById(storeId);
-
-        return reviews.stream()
-                .filter(review -> review.getRating() >= minRating && review.getRating() <= maxRating) // 별점 범위 필터링
-                .sorted(Comparator.comparing(Review::getCreatedAt).reversed()) // 최신순 정렬
-                .map(ReviewAddResponseDto::toDto) // DTO 변환
-                .collect(Collectors.toList());
+    public Page<ReviewDto> findByReviewsById(Long storeId, int minRating, int maxRating, Pageable pageable) {
+        Page<ReviewDto> reviewByStoreIdAndRating
+                = reviewRepository.findReviewByStoreIdAndRating(storeId, minRating, maxRating, pageable);
+        return reviewByStoreIdAndRating;
     }
-
 }
