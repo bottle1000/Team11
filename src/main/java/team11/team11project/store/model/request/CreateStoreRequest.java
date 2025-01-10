bc8ba06b9cc637e5b74a457824ea9c916eb24937
taejunUM <utj0411@naver.com>
2025-01-11 05:04:18 +0900

    fix. CreateStoreRequest. @NotBlank어노테이션 주석처리(todo check)

--- src/main/java/team11/team11project/store/model/request/CreateStoreRequest.java
+++ src/main/java/team11/team11project/store/model/request/CreateStoreRequest.java
@@ -14,11 +14,14 @@ public class CreateStoreRequest {
     @Size(min = 2, max = 20, message = "가게 이름은 2자 이상, 20자 이하여야합니다.")
     private String storeName;
 
-    @NotBlank(message = "오픈 시간 입력은 필수 입니다.")
+    // todo LocalTime에도 @NotBlank 사용이 안 됩니다ㅠ
+//    @NotBlank(message = "오픈 시간 입력은 필수 입니다.")
     private LocalTime openTime;
-    @NotBlank(message = "클로즈 시간 입력은 필수 입니다.")
+//    @NotBlank(message = "클로즈 시간 입력은 필수 입니다.")
     private LocalTime closeTime;
 
-    @NotBlank(message = "최소 주문 금액 입력은 필수 입니다.")
+    // todo int 값에는 @NotBlank 어노테이션을 사용할 수 없습니다!
+    // 사장님한테 최소 설정 금액을 요구하려면 @MIN 사용하세요
+//    @NotBlank(message = "최소 주문 금액 입력은 필수 입니다.")
     private int minOrderPrice;
 }
